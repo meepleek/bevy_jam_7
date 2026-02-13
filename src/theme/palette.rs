31f22cb201a2a7b14c9feb19f4651ac2f5e61b06
meepleek <ergocrab@gmail.com>
2026-02-13 13:58:10 +0100

    feat: neon-ish palette

--- src/theme/palette.rs
+++ src/theme/palette.rs
@@ -1,16 +1,47 @@
 use bevy::prelude::*;
 
 /// #ddd369
-pub const LABEL_TEXT: Color = Color::srgb(0.867, 0.827, 0.412);
+pub const LABEL_TEXT: Color = COL_LIGHT;
 
 /// #fcfbcc
-pub const HEADER_TEXT: Color = Color::srgb(0.988, 0.984, 0.800);
+pub const HEADER_TEXT: Color = COL_PURPLE;
 
 /// #ececec
-pub const BUTTON_TEXT: Color = Color::srgb(0.925, 0.925, 0.925);
+pub const BUTTON_TEXT: Color = COL_LIGHT;
 /// #4666bf
-pub const BUTTON_BACKGROUND: Color = Color::srgb(0.275, 0.400, 0.750);
+pub const BUTTON_BACKGROUND: Color = COL_PURPLE;
 /// #6299d1
-pub const BUTTON_FOCUSED_BACKGROUND: Color = Color::srgb(0.384, 0.600, 0.820);
+pub const BUTTON_FOCUSED_BACKGROUND: Color = COL_PURPLE_DARK;
 /// #3d4999
-pub const BUTTON_PRESSED_BACKGROUND: Color = Color::srgb(0.239, 0.286, 0.600);
+pub const BUTTON_PRESSED_BACKGROUND: Color = COL_PURPLE_DARKER;
+
+// most of the palette:
+// https://lospec.com/palette-list/chasm
+pub const COL_SNOW: Color = Color::srgb_u8(133, 218, 235);
+pub const COL_CYAN: Color = Color::srgb_u8(95, 201, 231);
+pub const COL_BLUE_LIGHT: Color = Color::srgb_u8(95, 161, 231);
+pub const COL_BLUEISH_PURPLE: Color = Color::srgb_u8(95, 110, 231);
+pub const COL_BLUE: Color = Color::srgb_u8(76, 96, 170);
+pub const COL_BLUE_DARK: Color = Color::srgb_u8(68, 71, 116);
+pub const COL_DARK: Color = Color::srgb_u8(50, 49, 59);
+pub const COL_PURPLE_DARKER: Color = Color::srgb_u8(70, 60, 94);
+pub const COL_PURPLE_DARK: Color = Color::srgb_u8(93, 71, 118);
+pub const COL_PURPLE: Color = Color::srgb_u8(133, 83, 149);
+pub const COL_PURPLE_LIGHT: Color = Color::srgb_u8(171, 88, 168);
+pub const COL_PINK: Color = Color::srgb_u8(202, 96, 174);
+pub const COL_ORANGE: Color = Color::srgb_u8(243, 167, 135);
+pub const COL_YELLOW: Color = Color::srgb_u8(245, 218, 167);
+pub const COL_GREEN_LIGHT: Color = Color::srgb_u8(141, 216, 148);
+pub const COL_GREEN: Color = Color::srgb_u8(93, 193, 144);
+pub const COL_GREEN_DARK: Color = Color::srgb_u8(74, 185, 163);
+pub const COL_TURQUOISE_DARK: Color = Color::srgb_u8(69, 147, 165);
+pub const COL_CYAN_NEON: Color = Color::srgb_u8(94, 253, 247);
+pub const COL_PINK_NEON: Color = Color::srgb_u8(255, 93, 204);
+pub const COL_YELLOW_NEON: Color = Color::srgb_u8(253, 254, 137);
+pub const COL_LIGHT: Color = Color::srgb_u8(255, 255, 255);
+
+// reds from
+// https://lospec.com/palette-list/cyberpunk-neons
+pub const COL_RED_NEON: Color = Color::srgb_u8(193, 17, 90);
+pub const COL_RED: Color = Color::srgb_u8(225, 58, 106);
+pub const COL_RED_LIGHT: Color = Color::srgb_u8(228, 106, 135);
